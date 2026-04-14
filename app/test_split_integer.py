b5b4154d0b8fa0e57fb3f75e232c118df663054d
Alexandr Titarenko <nook17n1@gmail.com>
2026-04-14 18:06:46 +0300

    Solution1

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,35 +2,29 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    goals = split_integer(17, 4)
-    assert sum(goals) == 17
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    goals = split_integer(6, 2)
-    assert goals == [3, 3]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    goals = split_integer(8, 1)
-    assert goals == [8]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    goals = split_integer(32, 6)
-    assert goals == sorted(goals)
+    assert split_integer(32, 6) == sorted(split_integer(32, 6))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    goals = split_integer(4, 5)
-    assert goals == [0, 1, 1, 1, 1]
+    assert split_integer(4, 5) == [0, 1, 1, 1, 1]
 
 
 def test_max_min_difference_at_most_one() -> None:
-    goals = split_integer(19, 3)
-    assert max(goals) - min(goals) <= 1
+    parts = split_integer(19, 3)
+    assert max(parts) - min(parts) <= 1
 
 
 def test_check_for_a_zero_value() -> None:
-    goals = split_integer(21, 0)
-    assert goals == []
+    assert split_integer(21, 0) == []
