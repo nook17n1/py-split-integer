c7925830af8e2b18dff740ff95ffd7dd34f83b5b
Alexandr Titarenko <nook17n1@gmail.com>
2026-04-14 13:01:16 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,35 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    goals = split_integer(17, 4)
+    assert sum(goals) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    goals = split_integer(6, 2)
+    assert goals == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    goals = split_integer(8, 1)
+    assert goals == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    goals = split_integer(32, 6)
+    assert goals == sorted(goals)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    goals = split_integer(4, 5)
+    assert goals == [0, 1, 1, 1, 1]
+
+
+def test_max_min_difference_at_most_one() -> None:
+    goals = split_integer(19, 3)
+    assert max(goals) - min(goals) <= 1
+
+
+def test_check_for_a_zero_value() -> None:
+    goals = split_integer(21, 0)
+    assert goals == []
